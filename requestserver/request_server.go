cb7ca38af157add224d2cab0b78206699e10d222
Philipp Czora <pczora@gmail.com>
2018-04-30 21:50:21 +0200

    Added file-based responses

--- requestserver/request_server.go
+++ requestserver/request_server.go
@@ -1,11 +1,15 @@
 package requestserver
 
 import (
+	"io/ioutil"
 	"log"
 	"net/http"
 
 	"github.com/pczora/dessert/requestmatcher"
 )
+
+// TODO: Make configurable
+const basePath = "./base/"
 
 type Response struct {
 	StatusCode int
@@ -16,11 +20,31 @@ type RequestServer struct {
 }
 
 func NewRequestServer() RequestServer {
-	endpoints := make(map[requestmatcher.RequestMatcher]Response)
-	// TODO: Read config from file/db/cli params/whatever
-
-	endpoints[requestmatcher.DefaultRequestMatcher{Path: "/test", Method: http.MethodGet}] = Response{http.StatusOK, "ok!"}
+	endpoints := parseFiles()
 	return RequestServer{endpoints}
+}
+
+func parseFiles() map[requestmatcher.RequestMatcher]Response {
+
+	endpoints := make(map[requestmatcher.RequestMatcher]Response)
+	files, err := ioutil.ReadDir(basePath)
+
+	if err != nil {
+		log.Println("Error reading directory")
+
+	}
+
+	for _, file := range files {
+		fileContent, err := ioutil.ReadFile(basePath + file.Name())
+		if err != nil {
+			log.Printf("Could not read file: %v", err)
+		}
+		// TODO: ATM it's GET only. What about the other methods?
+		endpoints[requestmatcher.DefaultRequestMatcher{Path: "/" + file.Name(), Method: http.MethodGet}] = Response{http.StatusOK, string(fileContent)}
+		log.Printf("Found file: %s", file.Name())
+	}
+
+	return endpoints
 }
 
 func (rs RequestServer) HandleRequest(w http.ResponseWriter, r *http.Request) {
